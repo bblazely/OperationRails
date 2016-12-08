b1677dc12ef3d704062d143ebccad78a4f9d959d
bblazely <ben@blazely.com>
2016-12-09 10:48:32 +1100

    * More serenity achieved. * Origin changed...

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,17 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+	attr_reader :values
+
+	def roll(numberOfDice)
+		@values = []
+		numberOfDice.downto(1) { |i|
+			@values.push(1 + rand(6))
+		}
+	end
+
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -39,8 +47,8 @@ class AboutDiceProject < Neo::Koan
 
     dice.roll(5)
     second_time = dice.values
-
-    assert_not_equal first_time, second_time,
+    
+assert_not_equal first_time, second_time,
       "Two rolls should not be equal"
 
     # THINK ABOUT IT:
